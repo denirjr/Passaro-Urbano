0bf66c696855488a11a5e990543df8c53320a229
denirjr <denir-junior19@live.com>
2018-03-20 21:42:56 -0300

    secao10 aula121

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
 
 @NgModule({
   declarations: [
@@ -19,16 +20,15 @@ import { DiversaoComponent } from './diversao/diversao.component';
     HomeComponent,
     RodapeComponent,
     RestaurantesComponent,
-    DiversaoComponent
+    DiversaoComponent,
+    OfertaComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -6,18 +6,18 @@ import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class OfertasService {
-
+    
     constructor(private http: Http){}
-
+    
     public getOfertas(): Promise<Oferta[]> {
         return this.http.get('http://localhost:3000/ofertas?destaque=true')
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
 
-    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]>{
-      return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
-        .toPromise()
-        .then((resposta: any) => resposta.json())
+    public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta: any) => resposta.json())
     }
 }
