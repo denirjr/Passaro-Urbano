9fcc6923f7bf0615148ed17bea16ea0db1cfffd1
denirjr <denir-junior19@live.com>
2018-04-02 22:11:09 -0300

    secao 11, aula 156

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable'
+import { Observable } from 'rxjs/Observable';
 
-import { OfertasService } from '../ofertas.service'
-import { Oferta } from '../shared/oferta.model'
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/of';
+
 
 @Component({
   selector: 'app-topo',
@@ -12,21 +18,29 @@ import { Oferta } from '../shared/oferta.model'
 })
 export class TopoComponent implements OnInit {
 
-  public ofertas: Observable<Oferta[]>
+  public ofertas: Observable<Oferta[]>;
+
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa // Retorno de Ofertas []
+    .debounceTime(1000) // Executa a ação do Switchmap apos um segundo
+    .distinctUntilChanged()
+    .switchMap((termo: string) => {
+      console.log('Requisição HTTP Man');
+      if (termo.trim() === '') {
+        return Observable.of<Oferta[]>([]);
+      }
+      return this.ofertasService.pesquisaOfertas(termo);
+    });
+    this.ofertas.subscribe((ofertas: Oferta []) => console.log(ofertas));
   }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
-    
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status: ', erro.status),
-      () => console.log('Fluxo de eventos completo!')
-    )
+    console.log('KeyUp Man', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca);
   }
 
 }
