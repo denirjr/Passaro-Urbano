817cec62528a03796d6d5efb5eef47941ef7d5c2
denirjr <denir-junior19@live.com>
2018-04-17 23:14:52 -0300

    secao 13 aula 178 - validacao de campos

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,23 +12,46 @@ export class OrdemCompraComponent implements OnInit {
   complemento: string = '';
   formaPagamento: string = '';
 
+  enderecoValido: boolean;
+  numeroValido: boolean;
+  complementoValido: boolean;
+  formaPagamentoValido: boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
   atualizaEndereco(endereco: any): void {
     this.endereco = endereco;
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
   atualizaNumero(numero: string): void {
     this.numero = numero;
+    if (this.numero.length > 0 ) {
+      this.numeroValido = true;
+  } else  {
+      this.numeroValido = false;
+    }
   }
 
   atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
+    if (this.complemento.length > 0) {
+      this.complementoValido = true;
+    }
   }
 
   atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 
 }
